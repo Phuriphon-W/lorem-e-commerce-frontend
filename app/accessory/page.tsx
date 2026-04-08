8ea0901a8e0bac8baef0e39730dc240cabcd6972
Phuriphon Wongsaengngam <phuriphon.wong@gmail.com>
2026-04-08 11:43:21 +0700

    add: apparels page

---
+++ app/accessory/page.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { getProducts } from "@/apis/product";
+import ProductCardContainer from "@/components/global/ProductCardContainer";
+import SearchBar from "@/components/global/SearchBar";
+import { Category } from "@/shared/enums/category";
+import { OrderBy } from "@/shared/enums/orderBy";
+import {
+  GetProductsResponse,
+  GetProductsRequest,
+} from "@/shared/types/product";
+import { Pagination } from "antd";
+import { useEffect, useState, Suspense } from "react"; // Added Suspense
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+
+function ProductContent() {
+  const router = useRouter();
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+
+  const [products, setProducts] = useState<GetProductsResponse>({
+    products: [],
+    total: 0,
+  });
+
+  const searchKeyword = searchParams.get("search") || "";
+  const page = Number(searchParams.get("page")) || 1;
+  const category = (searchParams.get("category") as Category) || Category.Empty;
+  const orderBy = (searchParams.get("orderBy") as OrderBy) || OrderBy.DateDsc;
+
+  const updateURL = (key: string, value: string | number) => {
+    const params = new URLSearchParams(searchParams.toString());
+    if (value) {
+      params.set(key, String(value));
+      if (key === "category") params.set("page", "1");
+    } else {
+      params.delete(key); 
+    }
+
+    router.push(`${pathName}?${params.toString()}`);
+  };
+
+  useEffect(() => {
+    const fetchProducts = async ({
+      pageNumber,
+      pageSize,
+      category,
+      search,
+      orderBy,
+    }: GetProductsRequest) => {
+      const response = await getProducts({
+        pageNumber,
+        pageSize,
+        category,
+        search,
+        orderBy,
+      });
+      setProducts(response);
+    };
+
+    fetchProducts({
+      pageNumber: page,
+      pageSize: 12,
+      category,
+      search: searchKeyword,
+      orderBy,
+    });
+  }, [searchKeyword, category, orderBy, page]);
+
+  return (
+    <div className="flex flex-col items-center py-10 w-full">
+      <h1 className="mb-4 font-medium text-[40px]">Our Products</h1>
+      <SearchBar
+        currentSearch={searchKeyword}
+        currentCategory={category}
+        currentOrderBy={orderBy}
+        onSearchChange={(val) => updateURL("search", val)}
+        onCategoryChange={(val) => {updateURL("category", val)}}
+        onOrderByChange={(val) => updateURL("orderBy", val)}
+      />
+      <ProductCardContainer columns={3} products={products.products} />
+      <Pagination 
+        total={products.total} 
+        pageSize={12} 
+        current={page} 
+        onChange={(val) => updateURL("page", val)} 
+        showQuickJumper 
+      />
+    </div>
+  );
+}
+
+export default function ProductPage() {
+  return (
+    <Suspense fallback={<div className="flex justify-center py-20 text-xl"></div>}>
+      <ProductContent />
+    </Suspense>
+  );
+}
--- components/global/SearchBar.tsx
+++ components/global/SearchBar.tsx
@@ -11,6 +11,8 @@ type SearchBarProps = {
   onSearchChange: (value: string) => void;
   onCategoryChange: (value: string) => void;
   onOrderByChange: (value: string) => void;
+  categorySelect?: boolean;
+  orderSelect?: boolean;
 };
 
 export default function SearchBar({
@@ -20,6 +22,8 @@ export default function SearchBar({
   onSearchChange,
   onCategoryChange,
   onOrderByChange,
+  categorySelect = true,
+  orderSelect = true,
 }: SearchBarProps) {
   return (
     <div className="w-[90%] flex flex-col md:flex-row gap-2">
@@ -32,31 +36,35 @@ export default function SearchBar({
       />
 
       {/* Category Filter */}
-      <Select
-        value={currentCategory} // Tie the displayed value to the URL state
-        className="w-full md:w-[15%]"
-        options={[
-          { value: "", label: "All Products" },
-          { value: Category.Apparel, label: "Apparels" },
-          { value: Category.Accessory, label: "Accessories" },
-        ]}
-        onChange={onCategoryChange}
-      />
+      {categorySelect && (
+        <Select
+          value={currentCategory} // Tie the displayed value to the URL state
+          className="w-full md:w-[15%]"
+          options={[
+            { value: "", label: "All Products" },
+            { value: Category.Apparel, label: "Apparels" },
+            { value: Category.Accessory, label: "Accessories" },
+          ]}
+          onChange={onCategoryChange}
+        />
+      )}
 
       {/* Order By Filter */}
-      <Select
-        value={currentOrderBy} // Tie the displayed value to the URL state
-        className="w-full md:w-[18%]"
-        options={[
-          { value: OrderBy.DateDsc, label: "Newest Arrivals" },
-          { value: OrderBy.DateAsc, label: "Oldest First" },
-          { value: OrderBy.NameAsc, label: "Name: A to Z" },
-          { value: OrderBy.NameDsc, label: "Name: Z to A" },
-          { value: OrderBy.PriceAsc, label: "Price: Low to High" },
-          { value: OrderBy.PriceDesc, label: "Price: High to Low" },
-        ]}
-        onChange={onOrderByChange}
-      />
+      {orderSelect && (
+        <Select
+          value={currentOrderBy} // Tie the displayed value to the URL state
+          className="w-full md:w-[18%]"
+          options={[
+            { value: OrderBy.DateDsc, label: "Newest Arrivals" },
+            { value: OrderBy.DateAsc, label: "Oldest First" },
+            { value: OrderBy.NameAsc, label: "Name: A to Z" },
+            { value: OrderBy.NameDsc, label: "Name: Z to A" },
+            { value: OrderBy.PriceAsc, label: "Price: Low to High" },
+            { value: OrderBy.PriceDesc, label: "Price: High to Low" },
+          ]}
+          onChange={onOrderByChange}
+        />
+      )}
     </div>
   );
 }
