f907a217f33b661c3e4a63083645f1bcf598b557
Phuriphon Wongsaengngam <phuriphon.wong@gmail.com>
2026-04-11 15:21:37 +0700

    add: empty cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -4,8 +4,9 @@ import { getCartByUserId } from "@/apis/cart";
 import CartTable from "@/components/cart/CartTable";
 import { useAuthContext } from "@/shared/hooks/useAuthContext";
 import { CartItem } from "@/shared/interfaces/cart";
+import { deleteCartItems } from "@/apis/cart";
 import { useEffect, useState, useCallback } from "react";
-import { message } from "antd";
+import { message, Button } from "antd";
 import axios from "axios";
 
 export default function CartPage() {
@@ -28,14 +29,52 @@ export default function CartPage() {
     }
   }, [userId]);
 
+  const emptyCart = async () => {
+    if (!userId) return;
+    if (cartItems.length === 0) {
+      message.error("The cart is already empty")
+      return;
+    }
+
+    const itemIds = cartItems.map((item) => item.productId)
+
+    try {
+      await deleteCartItems({ userId, productIds: itemIds });
+      await fetchCart();
+      message.success("All items removed from cart")
+    } catch (err) {
+      message.error("Failed to remove items from cart")
+    }
+  }
+
   // Initial Load
   useEffect(() => {
-    fetchCart()
+    fetchCart();
   }, [fetchCart]);
 
   return (
-    <div className="w-full flex flex-col items-center">
-      <CartTable cartItems={cartItems} userId={userId ?? ""} onRefresh={fetchCart} />
+    <div className="flex flex-col items-center py-10 w-full gap-4">
+      {/* Title */}
+      <h1 className="font-medium text-[40px]">Your Cart</h1>
+
+      {/* Cart Items Table */}
+      <CartTable
+        cartItems={cartItems}
+        userId={userId ?? ""}
+        onRefresh={fetchCart}
+      />
+
+      {/* Buttons */}
+      <div className="flex flex-row justify-between md:justify-end md:gap-4 w-[90%] px-4">
+        <div className="md:w-[20%] w-full">
+          <Button danger style={{ width: "100%" }} onClick={() => emptyCart()}>
+            Empty Cart
+          </Button>
+        </div>
+        <div className="md:w-[20%] w-full">
+          <Button style={{ width: "100%" }}>Checkout</Button>
+        </div>
+      </div>
     </div>
   );
 }
